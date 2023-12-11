8481d8cd202b6c5685e9e1478a40574fd58c0999
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-12-11 13:29:15 +0800

    DeptController, Slf4j - work

--- src/main/java/com/example/controller/DeptController.java
+++ src/main/java/com/example/controller/DeptController.java
@@ -1,7 +1,20 @@
 package com.example.controller;
 
+import com.example.pojo.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class DeptController {
+
+    //@RequestMapping(path = "/depts", method = RequestMethod.GET)
+    @GetMapping("/depts")
+    public Result listAll() {
+        log.info("Dept listAll() invoked");
+        return Result.success();
+    }
 }
