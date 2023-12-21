d6dda6faa2b6a166f0ae08d18b26f618c6a25ff1
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2023-12-21 16:13:10 +0800

    Emp - Paginated Queries 分页查询 - with PageHelper dependency

--- src/main/java/com/example/service/impl/EmpServiceImpl.java
+++ src/main/java/com/example/service/impl/EmpServiceImpl.java
@@ -4,8 +4,10 @@ import com.example.mapper.EmpMapper;
 import com.example.pojo.Emp;
 import com.example.pojo.EmpPageBean;
 import com.example.service.EmpService;
+import com.github.pagehelper.Page;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.github.pagehelper.PageHelper;
 
 import java.util.List;
 
@@ -15,16 +17,22 @@ public class EmpServiceImpl implements EmpService {
     @Autowired
     private EmpMapper empMapper;
 
+//    @Override
+//    public EmpPageBean page(Integer page, Integer pageSize) {
+//        Integer start = (page - 1) * pageSize;
+//        List<Emp> rows = empMapper.listPage(start, pageSize);
+//
+//        Long total = empMapper.getTotalRows();
+//
+//        return new EmpPageBean(total, rows);
+//    }
+
     @Override
     public EmpPageBean page(Integer page, Integer pageSize) {
-        Integer start = (page - 1) * pageSize;
-//        List<Emp> rows = empMapper.listPage((page - 1) * pageSize, pageSize);
-//        Parameter 'startPage' not found. Available parameters are [arg1, arg0, param1, param2]
-        List<Emp> rows = empMapper.listPage(start, pageSize);
-
-        Long total = empMapper.getTotalRows();
-
-        return new EmpPageBean(total, rows);
+        PageHelper.startPage(page, pageSize);
+        List<Emp> rows = empMapper.listAll();
+        Page<Emp> p = (Page<Emp>) rows;
+        return new EmpPageBean(p.getTotal(), p.getResult());
     }
 
 }
