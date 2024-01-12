c3fe7f10c992479e06d44409b8164efab7a97395
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2024-01-12 18:34:19 +0800

    LoginCheck - with Filter

--- src/main/java/com/example/SmsApplication.java
+++ src/main/java/com/example/SmsApplication.java
@@ -2,7 +2,10 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
+// scan servelet components, e.g., @WebServlet, @WebFilter, @WebListener
+@ServletComponentScan
 @SpringBootApplication
 public class SmsApplication {
 
