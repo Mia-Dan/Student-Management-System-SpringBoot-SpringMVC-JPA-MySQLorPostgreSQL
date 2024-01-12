c57d559c22c17a519ab6d7864e158126aa4a2229
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2024-01-12 18:36:00 +0800

    minor - Filter - change 1 line to comment out a filter

--- src/main/java/com/example/filter/LoginFilter.java
+++ src/main/java/com/example/filter/LoginFilter.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 
 /* Login Check Filter */
 @Slf4j
-@WebFilter(urlPatterns = "/*")
+//@WebFilter(urlPatterns = "/*")
 public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
