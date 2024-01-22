6fb95845d39f1aa675ffc9179988b010c1f34abf
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2024-01-22 21:07:46 +0800

    upload - max acceptable size - config

--- src/main/java/com/example/controller/UploadController.java
+++ src/main/java/com/example/controller/UploadController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
 import java.io.IOException;
 
 @Slf4j
@@ -19,7 +20,14 @@ public class UploadController {
 
     @PostMapping({"/upload"})
     public Result uploadToOSS(MultipartFile image) throws IOException {
-        String url = aliOSSUtils.upload(image);
+
+// A. save to local, tested with postman
+        image.transferTo(new File("D:\\a.zip"));
+        String url = "upload to local, test D:\\a.zip";
+
+// B. upload to OSS
+//        String url = aliOSSUtils.upload(image);
+
         log.info("upload to OSS, url: {}", url);
         return Result.success(url);
     }
