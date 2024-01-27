dd3122667298c7b8673b28e1e3ed4f7d1982af45
Mia-Dan <54273010+Mia-Dan@users.noreply.github.com>
2024-01-27 20:27:06 +0800

    minor / RestControllerAdvice ExceptionHandler comments

--- src/main/java/com/example/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/exception/GlobalExceptionHandler.java
@@ -4,8 +4,14 @@ import com.example.pojo.Result;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+// The term "advice" in ControllerAdvice is a concept from Aspect-Oriented Programming (AOP).
+// @ControllerAdvice: Specialization of @Component for classes // ref: src comment
+//      that declare @ExceptionHandler, @InitBinder, or @ModelAttribute methods
+//      to be shared across multiple @Controller classes.
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    // Annotation for handling exceptions in specific handler classes and/or handler methods.
     @ExceptionHandler(Exception.class) // catch all exceptions
     public Result error(Exception e) {
         e.printStackTrace();
